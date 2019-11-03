8b57b0ed2a79a776840a04e383f14a074f654c32
Kamil Jerzy Wojcik <kamil.jerzy.wojcik@gmail.com>
2019-11-03 15:55:35 +0100

    Model tefactor to regresion service

--- ML_lib/Services/ModelRegularization/LogisticRegresionClassifierTitanic.py
+++ ML_lib/Services/ModelRegularization/LogisticRegresionClassifierTitanic.py
@@ -1,15 +1,15 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from .Configuration import Configuration
 from .ModelRegularizationService import ModelRegularizationService
 
 
-
-class RandomForestClassifierTitanic:
+class LogisticRegresionClassifierTitanic:
 
     def __init__ (self):
         self.configuration = Configuration()
-        self.name_estymator = "RandomForestClassifier"
-        self.RegService = ModelRegularizationService(RandomForestClassifier(random_state=42))
+        self.estymator = LogisticRegression(random_state=42)
+        self.name_estymator = "LogisticRegresionClassifier"
+        self.RegService = ModelRegularizationService(LogisticRegression(random_state=42))
 
 
     def approximation(self, train_strat_num_titanic):
@@ -17,27 +17,31 @@ class RandomForestClassifierTitanic:
 
         param_grid_list = [
             {# default values
-                'n_estimators': [10], # int
-                'max_depth': [10], # int / None
-                'min_samples_split': [20], # int / float
-                'min_samples_leaf': [1], # int / float
-                'min_weight_fraction_leaf': [0.], # float
-                'max_features': ["auto"], # int/float/auto/sqrt/log2/None
-                'max_leaf_nodes': [None], # int / None
-                'min_impurity_decrease': [0.], # float
-                'bootstrap': [True],
-                'oob_score': [False], # if bootstrap true
+                'penalty': ['l1'], # ['l1', 'l2', 'elasticnet', 'none']
+                'dual': [False], # [True, False]
+                'tol': [1e-4], # float 
+                'C': [1.0], #float
+                'fit_intercept': [True],
+                'intercept_scaling': [1.0], # float
+                'class_weight': [None], # dct {class_label: weight}
+                'solver': ['liblinear'], # newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’
+                'max_iter': [100], # int
+                'multi_class': ['ovr'], # ‘ovr’, ‘multinomial’, ‘auto’
+                'verbose': [0], # int
+                'warm_start': [False],
+                'l1_ratio': [None] # float, None
             },
         ]
 
         print(f"{self.name_estymator} grid")
         dataframes = []
+
         for param_grid in param_grid_list:
             cv_results = self.RegService.get_grid_results(param_grid, X, y)
             result_grid_df = self.RegService.get_and_save_result_grid_dataframe(param_grid, cv_results, self.name_estymator)
             dataframes.append(result_grid_df)
+        
         return dataframes
-
 
     def approximation_random_grid(self, train_strat_num_titanic, n=100):
         X, y = self.RegService.get_X_and_y(train_strat_num_titanic)
@@ -65,4 +69,7 @@ class RandomForestClassifierTitanic:
             dataframes.append(result_grid_df)
         
         return dataframes
+    
+
+    
     
---
+++ ML_lib/Services/ModelRegularization/ModelRegularizationService.py
@@ -0,0 +1,77 @@
+from .Configuration import Configuration
+import pandas as pd
+import time
+from IPython.core.display import display
+import json
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
+
+class ModelRegularizationService:
+    def __init__(self, estymator):
+        self.configuration = Configuration()
+        self.estymator = estymator
+
+    def show_results(self, cv_results, param_show):
+        display_result = []
+        display_result.append(param_show)
+        for scoring, params in sorted(zip(cv_results["mean_test_score"], cv_results["params"]), key=lambda x: x[0], reverse=True):
+            params[self.configuration.scoring] = scoring
+            display_result.append(params)
+        result_df = pd.read_json(json.dumps(display_result))
+        display(result_df)
+        return result_df
+
+
+    def save_results(self, result_df, name):
+        current_time = time.time()
+        result_df.to_csv(f"RandomTreeClasifier/{name}_{current_time}.csv", index=False)
+
+
+    def get_X_and_y(self, train_strat_num_titanic):
+        X_train = train_strat_num_titanic.drop(columns=['Survived', 'PassengerId'], axis=1)
+        y_train = train_strat_num_titanic['Survived']
+        return X_train, y_train
+
+
+    def get_param_to_show(self, param_show):
+        param_show[self.configuration.scoring] = 'prediction'
+        for key in list(param_show.keys()):
+            param_show[key] = str(param_show[key])
+        return param_show
+
+
+    def get_and_save_result_grid_dataframe(self, param_grid, cv_results, name_estymator):
+        param_show = self.get_param_to_show(param_grid.copy())
+        result_grid_df = self.show_results(cv_results, param_show)
+        self.save_results(result_grid_df, name_estymator)
+        return result_grid_df
+
+
+    def get_grid_results(self, param_grid, X, y):
+            gridSearchCV = GridSearchCV(
+                    self.estymator, 
+                    [param_grid],
+                    cv=3,
+                    scoring=self.configuration.scoring, 
+                    return_train_score=True
+                )
+            gridSearchCV.fit(X, y)
+            return gridSearchCV.cv_results_
+
+
+    def get_random_grid_results(self, param_grid, X, y, n):
+        randomGridSearchCV = RandomizedSearchCV(
+            estimator=self.estymator, 
+            param_distributions=param_grid,
+            n_iter=n,
+            cv=3,
+            scoring=self.configuration.scoring, 
+            return_train_score=True
+        )
+        randomGridSearchCV.fit(X, y)
+        return randomGridSearchCV.cv_results_
+
+
+  
+
